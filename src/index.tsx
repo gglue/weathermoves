e1560b3af3c629c7c3e03a33575f87428e574a1e
gglue <generalglue@gmail.com>
2022-11-25 23:56:05 -0500

    Setup basic CSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/body.css';
+import './css/App.css';
+import './css/header.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
